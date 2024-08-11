53d8bb3c87f4de8c68c6cf39326f648d90a90630
Mohamed Ashraf <mhmdashrf1619@gmail.com>
2024-08-11 17:19:25 +0300

    Create packages.sh

---
+++ packages.sh
@@ -0,0 +1,12 @@
+sudo pacman -S --needed --noconfirm polkit polkit-gnome qt6-wayland qt5-wayland grim slurp nano xorg-xhost nm-connection-editor network-manager-applet ffmpeg \
+  hyprland hyprlock hyprpaper xdg-desktop-portal-hyprland \
+  thunar thunar-archive-plugin thunar-media-tags-plugin xarchiver thunar-volman ffmpegthumbnailer tumbler libgsf webp-pixbuf-loader gvfs gvfs-mtp gvfs-gphoto2 gvfs-smb sshfs \
+  git wget curl waybar unzip unrar xdg-user-dirs gnome-keyring brightnessctl playerctl pamixer pavucontrol font-manager gnome-disk-utility \
+  bluez blueberry \
+  eog gedit galculator gimp obs-studio vlc wl-clipboard cliphist rofi-wayland \
+  nwg-look qt6ct kvantum \
+  noto-fonts noto-fonts-emoji ttf-liberation ttf-dejavu ttf-firacode-nerd \
+  gnome-themes-extra epapirus-icon-theme \
+  nodejs-lts-iron yarn \
+  flatpak \
+  firefox
