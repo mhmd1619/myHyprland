f54a1d716f4111f942e099cf112dbd397e02ce33
Mohamed Ashraf <mhmdashrf1619@gmail.com>
2024-09-05 19:52:17 +0300

    Update aur_packages.sh

--- aur_packages.sh
+++ aur_packages.sh
@@ -1,1 +1,6 @@
-paru -S --noconfirm google-chrome visual-studio-code-bin hyprpicker nwg-displays wlogout auto-cpufreq waypaper ttf-ms-win11-auto
+paru -S --noconfirm google-chrome \
+  visual-studio-code-bin \
+  hyprpicker \
+  wlogout \
+  auto-cpufreq \
+  ttf-ms-win11-auto
