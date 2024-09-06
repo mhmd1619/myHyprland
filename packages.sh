2c299249e32473e94ec0975ab840a433b09c29f1
Mohamed Ashraf <mhmdashrf1619@gmail.com>
2024-09-06 16:45:21 +0300

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,9 +1,9 @@
-sudo pacman -S --needed --noconfirm polkit polkit-gnome qt6-wayland qt5-wayland grim slurp nano xorg-xhost nm-connection-editor network-manager-applet ffmpeg gdm \
-  hyprland hyprlock hyprpaper xdg-desktop-portal-hyprland \
+sudo pacman -S --needed --noconfirm polkit polkit-gnome nano man qt6-wayland qt5-wayland grim slurp nano xorg-xhost nm-connection-editor network-manager-applet ffmpeg gdm \
+  hyprland hyprlock swww xdg-desktop-portal-hyprland xwaylandvideobridge \
   thunar thunar-archive-plugin thunar-media-tags-plugin xarchiver thunar-volman ffmpegthumbnailer tumbler libgsf webp-pixbuf-loader gvfs gvfs-mtp gvfs-gphoto2 gvfs-smb sshfs \
-  git wget curl waybar unzip unrar xdg-user-dirs gnome-keyring brightnessctl playerctl pamixer pavucontrol font-manager gnome-disk-utility \
+  git wget curl waybar unzip unrar xdg-user-dirs gnome-keyring brightnessctl playerctl pamixer pavucontrol font-manager gnome-disk-utility zsh \
   bluez blueberry \
-  eog gedit galculator gimp obs-studio vlc wl-clipboard cliphist rofi-wayland kitty \
+  eog leafpad galculator gimp obs-studio vlc wl-clipboard cliphist rofi-wayland kitty \
   nwg-look qt6ct kvantum \
   noto-fonts noto-fonts-emoji ttf-liberation ttf-dejavu ttf-firacode-nerd \
   gnome-themes-extra epapirus-icon-theme \
