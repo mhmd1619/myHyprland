d29cc026328d4e496bf482301cf09dae0fc70847
Mohamed Ashraf <mhmdashrf1619@gmail.com>
2024-11-09 09:34:14 +0200

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,12 +1,12 @@
-sudo pacman -S --needed --noconfirm polkit polkit-gnome nano man qt6-wayland qt5-wayland grim slurp nano xorg-xhost nm-connection-editor network-manager-applet ffmpeg gdm dunst \
-  hyprland hyprlock swww xdg-desktop-portal-hyprland xwaylandvideobridge \
+sudo pacman -S --needed --noconfirm polkit polkit-gnome nano man qt6-wayland qt5-wayland grim slurp nano xorg-xhost nm-connection-editor network-manager-applet ffmpeg sddm dunst \
+  hyprland hyprlock swww xdg-desktop-portal-hyprland xwaylandvideobridge hyprpicker \
   thunar thunar-archive-plugin thunar-media-tags-plugin xarchiver thunar-volman ffmpegthumbnailer tumbler libgsf webp-pixbuf-loader gvfs gvfs-mtp gvfs-gphoto2 gvfs-smb sshfs \
   git wget curl waybar unzip unrar xdg-user-dirs gnome-keyring brightnessctl playerctl pamixer pavucontrol font-manager gnome-disk-utility zsh \
   bluez blueberry \
-  eog leafpad galculator gimp obs-studio vlc wl-clipboard cliphist rofi-wayland kitty \
-  nwg-look qt6ct kvantum \
+  eog leafpad galculator gimp obs-studio vlc wl-clipboard cliphist nwg-clipman wofi kitty \
+  nwg-look qt6ct \
+  upower power-profiles-daemon \
   noto-fonts noto-fonts-emoji ttf-liberation ttf-dejavu ttf-firacode-nerd \
-  gnome-themes-extra epapirus-icon-theme \
+  gnome-themes-extra adw-gtk-theme epapirus-icon-theme \
   nodejs-lts-iron yarn btop fastfetch \
-  flatpak \
   firefox
