84c0e5122331e2b538e01b34aa67171587d84dae
Mohamed Ashraf <mhmdashrf1619@gmail.com>
2024-11-09 11:58:55 +0200

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,7 +1,7 @@
 sudo pacman -S --needed --noconfirm polkit polkit-gnome nano man qt6-wayland qt5-wayland grim slurp nano xorg-xhost nm-connection-editor network-manager-applet ffmpeg sddm dunst \
   hyprland hyprlock swww xdg-desktop-portal-hyprland xwaylandvideobridge hyprpicker \
   thunar thunar-archive-plugin thunar-media-tags-plugin xarchiver thunar-volman ffmpegthumbnailer tumbler libgsf webp-pixbuf-loader gvfs gvfs-mtp gvfs-gphoto2 gvfs-smb sshfs \
-  git wget curl waybar unzip unrar xdg-user-dirs gnome-keyring brightnessctl playerctl pamixer pavucontrol font-manager gnome-disk-utility zsh \
+  git wget curl waybar unzip unrar xdg-user-dirs gnome-keyring brightnessctl playerctl pamixer pavucontrol font-manager gnome-disk-utility evince zsh \
   bluez blueberry \
   eog leafpad galculator gimp obs-studio vlc wl-clipboard cliphist nwg-clipman wofi kitty \
   nwg-look qt6ct \
